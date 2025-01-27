3ee3d28fbc4059b5a874369465e782cdaf3f58a0
Ali <ali.simsek035@gmail.com>
2025-01-27 22:46:08 +0300

    Added localstack config

--- docker/local/aws-init.sh
+++ docker/local/aws-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
-awslocal sqs create-queue --queue-name SqsDemoLocalUserSignUpDeadLetter.fifo --attributes '{"FifoQueue": "true"}'
-awslocal sqs create-queue --queue-name SqsDemoLocalUserSignUp.fifo --attributes '{"FifoQueue": "true", "RedrivePolicy": "{\"deadLetterTargetArn\":\"SqsDemoLocalUserSignUpDeadLetter.fifo\",\"maxReceiveCount\":\"3\"}"}'
+awslocal sqs create-queue --queue-name SqsDemoUserSignUpDeadLetter.fifo --attributes '{"FifoQueue": "true"}'
+awslocal sqs create-queue --queue-name SqsDemoUserSignUp.fifo --attributes '{"FifoQueue": "true", "RedrivePolicy": "{\"deadLetterTargetArn\":\"SqsDemoUserSignUpDeadLetter.fifo\",\"maxReceiveCount\":\"3\"}"}'
 set +x
