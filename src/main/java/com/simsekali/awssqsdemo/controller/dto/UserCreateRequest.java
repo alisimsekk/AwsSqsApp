631deea8ff4d44f18b3e7b9a3b80629ca841382b
Ali <ali.simsek035@gmail.com>
2025-02-01 22:29:32 +0300

    AWS SQS

--- src/main/java/com/simsekali/awssqsdemo/controller/dto/UserCreateRequest.java
+++ src/main/java/com/simsekali/awssqsdemo/controller/dto/UserCreateRequest.java
@@ -18,4 +18,5 @@ public class UserCreateRequest {
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
+    private boolean deadLetterQueueTest;
 }
