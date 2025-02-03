08e4d6ec7c253452bc42a11f58441ee270f5da9e
Ali <ali.simsek035@gmail.com>
2025-02-04 00:34:05 +0300

    AWS SQS

--- src/main/java/com/simsekali/awssqsdemo/repository/UserRepository.java
+++ src/main/java/com/simsekali/awssqsdemo/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.simsekali.awssqsdemo.repository;
 
-import com.simsekali.awssqsdemo.controller.dto.UserDto;
 import com.simsekali.awssqsdemo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
